abed19b533978e9ad3ea85eee36c0cd841fcc479
Seldom <ssaguUUUU@gmail.com>
2020-02-12 22:23:46 -0700

    Simple Player Movement

---
+++ Assets/Scripts/GroundCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GroundCollider : MonoBehaviour
+{
+    private PlayerMove parentMove;
+
+    private void Start()
+    {
+        parentMove = transform.parent.GetComponent<PlayerMove>();
+    }
+
+    public void OnTriggerStay2D(Collider2D _)
+    {
+        // Tell parent it's on ground
+        parentMove.Ground();
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    public float jumpVelocity;
+    public int coyoteTime;
+
+    private int groundedTimer;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Horiontal movement
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0) * speed;
+
+        // Jumping
+        if (groundedTimer > 0)
+        {
+            if (Input.GetButtonDown("Jump") && groundedTimer > 0)
+            {
+                // Jump
+                rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+                groundedTimer = 0;
+            }
+            else
+                groundedTimer--;
+        }
+    }
+
+    public void Ground()
+    {
+        groundedTimer = coyoteTime;
+    }
+}
