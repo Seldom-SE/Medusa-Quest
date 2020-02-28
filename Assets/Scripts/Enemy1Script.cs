27c3a3172d913a3f8a1574405ca1f60dec4e7aa6
amtrang <61100945+amtrang@users.noreply.github.com>
2020-02-27 18:02:33 -0700

    Finished Some Enemy AI

---
+++ Assets/Scripts/Enemy1Script.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1Script : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+
+    // Change from 'right' to 'left' if the enemy should start going left
+    private bool movingRight = true;
+
+    public Transform wallDetection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Change from 'right' to 'left' if the enemy should start going left
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D wallInfo = Physics2D.Raycast(wallDetection.position, Vector2.right, distance);
+        if (wallInfo.collider == true)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+                distance *= -1;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+                distance *= -1;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy2Script.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2Script : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+
+    private bool movingRight = true;
+
+    private float frames;
+
+    public Transform wallDetection;
+
+    void Start()
+    {
+        frames = Time.frameCount;
+    }
+
+    void turning()
+    {
+        if (movingRight == true)
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            movingRight = false;
+            distance *= -1;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            movingRight = true;
+            distance *= -1;
+        }
+    }
+
+    void Update()
+    {
+        frames = Time.frameCount;
+
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D wallInfo = Physics2D.Raycast(wallDetection.position, Vector2.right, distance);
+        if (wallInfo.collider == true)
+        {
+            turning();
+        }
+
+        if (frames % 180 == 0)
+        {
+            turning();
+        }
+    }
+}
+
