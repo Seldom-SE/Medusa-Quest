325cb9b715029c1d6d5a946c3061a6c93b9cfdf8
Seldom <ssaguUUUU@gmail.com>
2020-03-05 12:44:51 -0700

    Basic Portables

---
+++ Assets/Scripts/Portable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Portable : MonoBehaviour
+{
+    public void BottomEnter(Collision2D collision)
+    {
+        if (transform.parent == null && collision.gameObject.GetComponent<Portable>() != null)
+        {
+            transform.parent = collision.transform;
+        }
+    }
+
+    public void BottomExit(Collision2D collision)
+    {
+        if (collision.transform == transform.parent)
+        {
+            transform.parent = null;
+        }
+    }
+}
---
+++ Assets/Scripts/PortableBottom.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PortableBottom : MonoBehaviour
+{
+    public Portable parent;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        parent.BottomEnter(collision);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        parent.BottomExit(collision);
+    }
+}
